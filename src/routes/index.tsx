cdd57efda854b465e3c1c6877a9057ccf09afdb2
Francisco <javiermartincuenca@gmail.com>
2024-05-26 21:01:29 +0200

    Connect to supabase

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,25 +1,43 @@
-import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
+import { component$ } from '@builder.io/qwik';
+import { routeLoader$, type DocumentHead } from '@builder.io/qwik-city';
+import { createServerClient } from 'supabase-auth-helpers-qwik';
+
+type TestRow = {
+  id: number;
+  created_at: string;
+  test_column: string;
+};
+
+export const useTestTable = routeLoader$(async (requestEv) => {
+  const supabaseClient = createServerClient(
+    requestEv.env.get('VITE_SUPABASE_URL')!,
+    requestEv.env.get('VITE_SUPABASE_ANON_KEY')!,
+    requestEv
+  );
+
+  const { data } = await supabaseClient.from('test').select('*');
+  console.log(data);
+  return data as TestRow[];
+});
 
 export default component$(() => {
+  const testTableRows = useTestTable();
   return (
     <>
-      <h1>Hi 👋</h1>
-      <div>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </div>
+      <h1>Hi from Supabase👋</h1>
+      {testTableRows.value.map((row) => (
+        <div key={row.id}>{row.test_column}</div>
+      ))}
     </>
   );
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
+  title: 'Welcome to Qwik',
   meta: [
     {
-      name: "description",
-      content: "Qwik site description",
+      name: 'description',
+      content: 'Qwik site description',
     },
   ],
 };
