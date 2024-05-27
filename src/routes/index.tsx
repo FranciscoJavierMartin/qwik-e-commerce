e10c3ffec03662cc2ab4ed038ec82c6098917de8
Francisco <javiermartincuenca@gmail.com>
2024-05-27 17:41:30 +0200

    Show products

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,18 @@
 import { component$ } from '@builder.io/qwik';
-import { routeLoader$, type DocumentHead } from '@builder.io/qwik-city';
+import {
+  routeLoader$,
+  useNavigate,
+  type DocumentHead,
+} from '@builder.io/qwik-city';
 import { supabaseClient } from '~/utils/supabase';
+
+type Product = {
+  id: number;
+  name: string;
+  description: string;
+  price: string;
+  image: string;
+};
 
 export const useUser = routeLoader$(async (requestEv) => {
   const supabaseAccessToken = requestEv.cookie.get('supabase_access_token');
@@ -16,13 +28,52 @@ export const useUser = routeLoader$(async (requestEv) => {
   return error ? null : data.user;
 });
 
+export const useProducts = routeLoader$(async () => {
+  const { data } = await supabaseClient.from('products').select('*');
+  return data as Product[];
+});
+
 export default component$(() => {
-  const useSig = useUser();
+  const userSig = useUser();
+  const productsSig = useProducts();
+  const navigate = useNavigate();
+
   return (
-    <>
-      <h1>Hi from Supabase👋</h1>
-      <h2>Welcome {useSig.value ? useSig.value.email : 'guest'}</h2>
-    </>
+    <div>
+      {productsSig.value.map((product) => (
+        <div key={product.id}>
+          <div>
+            <div>
+              <img
+                loading='eager'
+                width={280}
+                height={280}
+                src={`/images/${product.image}`}
+                alt={product.name}
+              />
+            </div>
+          </div>
+          <div>
+            <span>{product.name}</span>
+            <div>{product.description}</div>
+            <div>
+              <span>$ {product.price}</span>
+              {userSig.value ? (
+                <button type='button' class=''
+                onClick$={() => {console.log('Add to cart')}}>
+                  <IconShoppingCart/>
+                  Add to cart
+                  </button>
+              ):(
+                <button type='button' class='' onClick$={() => navigate('/sign-in')}>
+                  Sign In
+                </button>
+              )}
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
   );
 });
 
