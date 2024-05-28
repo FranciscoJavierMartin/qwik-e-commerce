7438a4ad3644664e7917457d6158b4feb3bdc707
Francisco <javiermartincuenca@gmail.com>
2024-05-28 18:30:23 +0200

    Add store

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,7 +1,22 @@
-import { component$, Slot } from '@builder.io/qwik';
+import {
+  component$,
+  createContextId,
+  Slot,
+  useContextProvider,
+  useStore,
+} from '@builder.io/qwik';
 import { routeLoader$ } from '@builder.io/qwik-city';
 import NavbarTop from '~/components/NavbarTop';
+import { type Store } from '~/utils/store';
 import { supabaseClient } from '~/utils/supabase';
+
+export const STORE_CONTEXT = createContextId<Store>('STORE_CONTEXT');
+
+const initialData: Store = {
+  cart: {
+    products: [],
+  },
+};
 
 export const useUser = routeLoader$(async (requestEv) => {
   const supabaseAccessToken = requestEv.cookie.get('supabase_access_token');
@@ -11,13 +26,16 @@ export const useUser = routeLoader$(async (requestEv) => {
   }
 
   const { data, error } = await supabaseClient.auth.getUser(
-    supabaseAccessToken.value
+    supabaseAccessToken.value,
   );
 
   return error ? null : data.user;
 });
 
 export default component$(() => {
+  const store = useStore<Store>(initialData, { deep: true });
+  useContextProvider(STORE_CONTEXT, store);
+
   return (
     <div>
       <NavbarTop />
